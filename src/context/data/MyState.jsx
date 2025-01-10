ae25b4626cfa2e16b00cd8b956725fe7339b311e
yash bhidawe <ybh5179@gmail.com>
2025-01-10 22:36:47 +0530

    fixing deploymnent error firebase import

--- src/context/data/MyState.jsx
+++ src/context/data/MyState.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { fireDB } from "../../firebase/firebaseConfig";
+import { fireDB } from "../../firebase/firebaseConfig.jsx";
 import {
   Timestamp,
   addDoc,
