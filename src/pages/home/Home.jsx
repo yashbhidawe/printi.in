370b1af21a4eb6f4fdd7adb864266564ef7173c6
yash bhidawe <ybh5179@gmail.com>
2025-01-10 21:08:09 +0530

    Successful local build

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 
 import Layout from "../../components/layout/Layout";
-import MyContext from "../../context/data/myContext";
 import Hero from "../../components/heroSection/HeroSection";
 import Filter from "../../components/filter/Filter";
 import ProductCard from "../../components/productCard/ProductCard";
