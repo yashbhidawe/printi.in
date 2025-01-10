30dc5f92d6bd0f0989007b6939f375f4da550e69
yash bhidawe <ybh5179@gmail.com>
2025-01-10 23:24:24 +0530

    cleared unused declarations

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "../Navbar/Navbar.jsx";
-import Footer from "../Footer/Footer.jsx";
+import Navbar from "../navbar/Navbar.jsx";
+import Footer from "../footer/Footer.jsx";
 
 function Layout({ children }) {
   return (
